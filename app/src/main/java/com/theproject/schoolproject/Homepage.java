1ed2a5c234c31d57ced95f80229c7f6a916d4722
linoy18 <46220147+linoy18@users.noreply.github.com>
2020-11-01 16:36:21 +0200

    first commit

--- app/src/main/java/com/theproject/schoolproject/Homepage.java
+++ app/src/main/java/com/theproject/schoolproject/Homepage.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Homepage extends AppCompatActivity {
-
+    //Linoys first commit 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
