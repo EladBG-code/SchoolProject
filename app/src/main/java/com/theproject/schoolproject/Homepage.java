478ed129e43222dc0333b495332c91e380d583bf
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2020-11-02 21:31:45 +0200

    commit 17:47 2/11/20

--- app/src/main/java/com/theproject/schoolproject/Homepage.java
+++ app/src/main/java/com/theproject/schoolproject/Homepage.java
@@ -1,15 +1,26 @@
 package com.theproject.schoolproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
-public class Homepage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Homepage extends AppCompatActivity{
 
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        //toolbar = findViewById(R.id.);
     }
 }
