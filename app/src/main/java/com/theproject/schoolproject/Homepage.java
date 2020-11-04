21cba62148f660a9f51cae06e01468851e0d412c
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2020-11-04 19:00:20 +0200

    hamburger menu working

--- app/src/main/java/com/theproject/schoolproject/Homepage.java
+++ app/src/main/java/com/theproject/schoolproject/Homepage.java
@@ -3,6 +3,7 @@ package com.theproject.schoolproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -28,6 +29,7 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
         toolbar = findViewById(R.id.toolbarHome);
 
         setSupportActionBar(toolbar);
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
@@ -39,5 +41,17 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //This function makes sure the app doesn't close down if someone is back pressing in their phone
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
     }
 }
