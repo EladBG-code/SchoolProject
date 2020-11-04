43063484e50106b48e2064a3f7e28314cab824de
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2020-11-04 13:17:57 +0200

    commit 13:17 4/11/20

--- app/src/main/java/com/theproject/schoolproject/Homepage.java
+++ app/src/main/java/com/theproject/schoolproject/Homepage.java
@@ -1,11 +1,13 @@
 package com.theproject.schoolproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity{
@@ -13,7 +15,7 @@ public class Homepage extends AppCompatActivity{
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
+    MaterialToolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,8 @@ public class Homepage extends AppCompatActivity{
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbarHome);
 
-
+        setSupportActionBar(toolbar);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar);
 
 
     }
