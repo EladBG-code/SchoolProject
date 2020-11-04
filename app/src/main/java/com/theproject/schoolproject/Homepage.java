bffd0b576c42febeb6d2d61742e9b707f7dc077f
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2020-11-04 13:50:02 +0200

    commit 13:49 4/11/20

--- app/src/main/java/com/theproject/schoolproject/Homepage.java
+++ app/src/main/java/com/theproject/schoolproject/Homepage.java
@@ -1,16 +1,18 @@
 package com.theproject.schoolproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
-public class Homepage extends AppCompatActivity{
+public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     DrawerLayout drawerLayout;
@@ -26,8 +28,16 @@ public class Homepage extends AppCompatActivity{
         toolbar = findViewById(R.id.toolbarHome);
 
         setSupportActionBar(toolbar);
-        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
