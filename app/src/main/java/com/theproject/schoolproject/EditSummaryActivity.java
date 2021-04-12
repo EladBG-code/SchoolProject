3800cbf2dd319318e610acf1e4e10436f2a7d509
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2021-04-12 20:13:48 +0300

    Likes added to viewsummary cards ranges from 0-5 (counted) 10-20 (10+) 20-40 (20+) 40-60 (40+) 60-80 (60+) 80-100 (80+) 100+ (100+)
    ------------
    added edit summary acitivity

---
+++ app/src/main/java/com/theproject/schoolproject/EditSummaryActivity.java
@@ -0,0 +1,14 @@
+package com.theproject.schoolproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_summary);
+    }
+}
